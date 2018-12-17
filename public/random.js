f60aa93d06fc6e8c02ba6df77d2098313ae7ac7c
Pedro J. Molina <pjmolina@gmail.com>
2018-12-17 17:24:12 +0100

    fixed ramdom. close #1

--- public/random.js
+++ public/random.js
@@ -6,13 +6,13 @@ function clearAll() {
     renderNumber('-');
 }
 
-function generateRandomNumber() {
-    return 9;
-}
-
-// function generateRandomNumber_Version2() {
-//     return Math.floor(Math.random() * 10);
+// function generateRandomNumber() {
+//     return 9;
 // }
+
+function generateRandomNumber_Version2() {
+    return Math.floor(Math.random() * 10);
+}
 
 function renderNumber(text) {
     document.getElementById("generado").innerHTML = text;
