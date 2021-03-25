4cf0fc5dd31d58e6eb21603c86dd324533792788
rodcalrub <rodcalrub@alum.us.es>
2021-03-25 19:30:14 +0100

    Updated with passwd

---
+++ terraform/machine.tf
@@ -0,0 +1,69 @@
+resource "aws_instance" "alea" {
+  ami                         = "ami-0d9a499b43edd7ae0" // "ami-2757f631"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.PRACTICAS_DEVOPS.id]
+
+  root_block_device {
+    volume_size = 20 #20 Gb
+  }
+
+  tags = {
+    Name        = "${var.author}.alea"
+    Author      = var.author
+    Date        = "2020.03.25"
+    Environment = "LAB"
+    Location    = "Paris"
+    Project     = "PRACTICAS_DEVOPS"
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.key_path)
+  }
+
+  # provisioner "file" {
+  #   source      = "file1.txt"
+  #   destination = "/home/ec2-user/file1.txt"
+  # }
+  provisioner "file" {
+    content     = <<EOF
+{
+    "log-driver": "awslogs",
+    "log-opts": {
+      "awslogs-group": "docker-logs-test",
+      "tag": "{{.Name}}/{{.ID}}"
+    }
+}
+EOF
+    destination = "/home/ec2-user/daemon.json"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y docker httpd-tools",
+      "sudo usermod -a -G docker ec2-user",
+      "sudo curl -L https://github.com/docker/compose/releases/download/1.22.0-rc2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      "sudo chkconfig docker on",
+      "sudo service docker start",
+      # portainer
+      # "sudo bash ../docker-build.sh",
+      # "sudo bash ../docker-run.sh",
+      # "sudo docker run -d --name Alea -p 80:80 rodrasna/alea",
+      "npm install",
+      "npm start",
+      "echo -n ${var.portainer_passwd} > /tmp/portainer_passwd",
+      "docker run -d --name portainer -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/portainer_passwd:/tmp/portainer_passwd portainer/portainer-ce --admin-password-file /tmp/portainer_passwd",
+    ]
+  }
+
+  # provisioner "remote-exec" {
+  #   script = "./launch-containers.sh"
+  # }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,40 @@
+# variable "access_key" {
+#     description = "AWS access key"
+#     type        = string
+#     default     = ""
+# }
+
+# variable "secret_key" {
+#     description = "AWS secret key"
+#     type        = string
+#     default     = ""
+# }
+
+variable "author" {
+  description = "Name of the operator. Used as a prefix to avoid name collision on resources."
+  type        = string
+  default     = "rodcalrub"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-2" # Paris
+}
+
+variable "key_path" {
+  description = "Key path for SSHing into EC2"
+  type        = string
+  default     = "../keys/paris-keys.pem"
+}
+
+variable "key_name" {
+  description = "Key name for SSHing into EC2"
+  type        = string
+  default     = "paris-keys"
+}
+
+variable "portainer_passwd" {
+  description = "Portainer admin passwd"
+  type = string
+}
